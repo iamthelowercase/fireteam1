25a3c209abe2f3b346ae950d70371e5ce1992f90
KD Wakeman <kdw1992@fastmail.com>
2025-07-16 21:23:16 -0400

    UNBUILDABLE: forgot to commit FOV update
    Field of View was working, I just forgot to committ that before moving on to the next thing.

--- engine.py
+++ engine.py
@@ -1,7 +1,8 @@
-from typing import Set, Iterable, Any
+from typing import Iterable, Any
 
 from tcod.context import Context
 from tcod.console import Console
+from tcod.map import compute_fov
 
 from entity import Entity
 from input_handlers import EventHandler
@@ -9,11 +10,11 @@ from game_map import GameMap
 
 
 class Engine:
-    def __init__(self, entities: Set[Entity], event_handler: EventHandler, game_map: GameMap, player: Entity):
-        self.entities = entities
+    def __init__(self, event_handler: EventHandler, game_map: GameMap, player: Entity):
         self.event_handler = event_handler
         self.player = player
         self.game_map = game_map
+        self.update_fov()
 
     def handle_events(self, events: Iterable[Any]) -> None:
         for event in events:
@@ -23,12 +24,29 @@ class Engine:
                 continue
 
             action.perform(self, self.player)
+            self.update_fov() # update the FOV after every player action
+
+    def update_fov(self) -> None:
+        """ Recompute the visible area based on the player's point of view."""
+        self.game_map.visible[:] = compute_fov(
+            self.game_map.tiles["transparent"],
+            (self.player.x, self.player.y),
+            radius=12, # this number controls how far the player characters can see
+            light_walls=True, # added as part of experimenting with FOV algorithms; unneeded otherwise
+            algorithm=0,
+            # agorthim = 0 # a basic ray casting implementation that I kind of like
+            # see https://python-tcod.readthedocs.io/en/latest/tcod/map.html#tcod.map.compute_fov for more options, although not well explained how to invoke them
+        )
+        # if a tile is "visible" it should be added to "explored".
+        self.game_map.explored |= self.game_map.visible
 
     def render(self, console: Console, context: Context) -> None:
         self.game_map.render(console)
         
         for entity in self.entities:
-            console.print(entity.x, entity.y, entity.char, fg=entity.color)
+            # only print entities that are in the FOV
+            if self.game_map.visible[entity.x, entity.y]:
+                console.print(entity.x, entity.y, entity.char, fg=entity.color)
 
         context.present(console)
 
--- game_map.py
+++ game_map.py
@@ -1,12 +1,23 @@
+from __future__ import annotations
+from typing import Iterable, TYPE_CHECKING
 import numpy as np
 from tcod.console import Console
 
 import tile_types
 
+if TYPE_CHECKING:
+	from entity import Entity
+
 class GameMap:
-	def __init__(self, width: int, height: int):
+	def __init__(self, width: int, height: int, entities: Iterable[Entity] = ()):
 		self.width, self.height = width, height # remember to change this line if you change the ordering on the console
+		self.entities = set(entities)
 		self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F")
+		
+		self.visible = np.full((width, height), fill_value=False, order="F")
+			# tiles the player can see currently
+		self.explored = np.full((width, height), fill_value=False, order="F")
+			# tiles the player has seen before, but can't see currently
 	
 	def in_bounds(self, x: int, y: int) -> bool:
 		"""Return true if x and y are inside the bounds of this map."""
@@ -14,4 +25,10 @@ class GameMap:
 		# that is a gnarly inlined return but it's so good that it *is* inlined
 	
 	def render(self, console: Console) -> None:
-		console.rgb[0:self.width, 0:self.height] = self.tiles["dark"]
+		"""Renders the map.
+		"""
+		console.rgb[0:self.width, 0:self.height] = np.select(
+			condlist=[self.visible, self.explored],
+			choicelist=[self.tiles["light"], self.tiles["dark"]],
+			default=tile_types.SHROUD
+		)
